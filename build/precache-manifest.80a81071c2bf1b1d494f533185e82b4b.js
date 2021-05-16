a70ebd12fe764e257b09f8462836c66ba2f2ebbf
skylanskylion <skylanskylion@outlook.com>
2021-05-16 18:02:23 +0530

    added build for netflify

---
+++ build/precache-manifest.80a81071c2bf1b1d494f533185e82b4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c98dc4eb36d331599338789a9df2d63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "576ebd760c6cd7042685",
+    "url": "/static/js/2.ff869181.chunk.js"
+  },
+  {
+    "revision": "2a3a23bf878d7187dc1e20049a44e9ba",
+    "url": "/static/js/2.ff869181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25e65c1edbd9d73c6a9",
+    "url": "/static/js/main.21c7cb62.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "cac9c10c001d2b38a4f86e33da2f4fb3",
+    "url": "/static/media/cart.cac9c10c.svg"
+  }
+]);
